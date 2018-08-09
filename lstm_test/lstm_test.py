0f38bb5a15b08b58fc693031816c48eda115ac02
qiguoluan <1484836966@qq.com>
2018-08-09 22:56:22 +0800

    添加停用词典

---
+++ lstm_test/lstm_test.py
@@ -0,0 +1,127 @@
+#! /bin/env python
+# -*- coding: utf-8 -*-
+"""
+预测
+"""
+import jieba
+import numpy as np
+from gensim.models.word2vec import Word2Vec
+from gensim.corpora.dictionary import Dictionary
+from keras.preprocessing import sequence
+
+import yaml
+from keras.models import model_from_yaml
+np.random.seed(1337)  # For Reproducibility
+import sys
+sys.setrecursionlimit(1000000)
+
+# 定义参数
+maxlen = 100
+
+def create_dictionaries(model=None,
+                        combined=None):
+    ''' Function does are number of Jobs:
+        1-创建索引映射的单词
+        2-创建一个单词到矢量映射
+        3-转换训练和测试词典
+
+    '''
+    if (combined is not None) and (model is not None):
+        gensim_dict = Dictionary()
+        gensim_dict.doc2bow(model.wv.vocab.keys(),
+                            allow_update=True)
+        #  freqxiao10->0 所以k+1
+        w2indx = {v: k+1 for k, v in gensim_dict.items()}#所有频数超过10的词语的索引,(k->v)=>(v->k)
+        w2vec = {word: model[word] for word in w2indx.keys()}#所有频数超过10的词语的词向量, (word->model(word))
+
+        def parse_dataset(combined): # 闭包-->临时使用
+            ''' Words become integers
+            '''
+            data=[]
+            for sentence in combined:
+                new_txt = []
+                for word in sentence:
+                    try:
+                        new_txt.append(w2indx[word])
+                    except:
+                        new_txt.append(0) # freqxiao10->0
+                data.append(new_txt)
+            return data # word=>index
+        combined=parse_dataset(combined)
+        combined= sequence.pad_sequences(combined, maxlen=maxlen)#每个句子所含词语对应的索引，所以句子中含有频数小于10的词语，索引为0
+        return w2indx, w2vec,combined
+    else:
+        print ('没有提供数据...')
+
+def loadStopWords():   
+    
+    stop = [line.strip()  for line in open('./stopWords.txt', 'r', encoding='utf-8').readlines() ]   
+    #print("type(loadStopWords_stop)",type(stop))
+    return stop  
+
+def input_transform(string):
+    words=jieba.lcut(string)
+    '''
+    stopWords = loadStopWords()
+    leftWords = []   
+    for i in words: 
+        if (i not in stopWords):  
+            leftWords.append(i)
+    text_str = ' '.join(leftWords)
+    '''
+    words=np.array(words).reshape(1,-1)
+    #载入模型
+    model=Word2Vec.load('./Word2vec_model.pkl')
+    _,_,combined=create_dictionaries(model,words)
+    return combined
+
+
+def lstm_predict(string):
+    #print ('loading model...')
+    with open('./lstm.yml', 'r') as f:
+        yaml_string = yaml.load(f)
+    model = model_from_yaml(yaml_string)
+
+    #print ('loading weights...')
+    model.load_weights('./lstm.h5')
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',metrics=['accuracy'])
+
+    neg = 0
+    neu = 0
+    pos = 0
+    sum = 0
+    with open(string ,'r', encoding='utf-8') as ff:
+        for line in ff.readlines():
+            data=input_transform(line)
+            data.reshape(1,-1)
+            #print data
+            result=model.predict_classes(data)
+            choose = result[0]
+            print(sum+1,choose)
+            if choose==1:
+                pos += 1
+                sum += 1
+            elif choose==0:
+                neu += 1
+                sum += 1
+            else:
+                neg += 1
+                sum += 1
+    print("sum",sum)
+    print("negative:",neg)
+    print("neural",neu)
+    print("positive",pos)
+    print('negative/sum: {:.2%}'.format(neg/sum))
+    print('neural/sum: {:.2%}'.format(neu/sum))
+    print('positive/sum: {:.2%}'.format(pos/sum))
+
+
+if __name__=='__main__':
+    
+    string = './result_test.txt'
+    lstm_predict(string)       
+
+
+
+    
---
+++ lstm_test/lstm_train.py
@@ -0,0 +1,247 @@
+#! /bin/env python
+# -*- coding: utf-8 -*-
+"""
+训练网络，并保存模型，其中LSTM的实现采用Python中的keras库
+"""
+import pandas as pd 
+import numpy as np 
+import jieba
+import multiprocessing
+import keras
+
+from gensim.models.word2vec import Word2Vec
+from gensim.corpora.dictionary import Dictionary
+from keras.preprocessing import sequence
+
+from sklearn.cross_validation import train_test_split
+from keras.models import Sequential
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import LSTM, GRU
+from keras.layers.core import Dense, Dropout,Activation
+from keras.models import model_from_yaml
+
+from keras.layers import SimpleRNN
+
+np.random.seed(1337)  # For Reproducibility
+import sys
+sys.setrecursionlimit(1000000)
+import yaml
+#jieba中添加专业词典
+jieba.load_userdict('./introduction.txt') 
+# 设置参数
+cpu_count = multiprocessing.cpu_count() # 4
+vocab_dim = 100 #向量维度
+n_iterations = 5  # 理想情况更多..
+n_exposures = 5 # 所有频数超过10的词语
+window_size = 7
+n_epoch = 6
+input_length = 100
+maxlen = 100 #文本保留的最大长度
+
+batch_size = 32
+
+
+def loadfile():
+    neg=pd.read_csv('./neg.csv',header=None,index_col=None,error_bad_lines=False)
+    pos=pd.read_csv('./pos.csv',header=None,index_col=None,error_bad_lines=False)
+    neu=pd.read_csv('./neu.csv', header=None, index_col=None,error_bad_lines=False)
+
+    #数组拼接
+    combined = np.concatenate((pos[0], neu[0], neg[0]))
+    #标签拼接
+    y = np.concatenate((np.ones(len(pos), dtype=int), np.zeros(len(neu), dtype=int), 
+                        -1*np.ones(len(neg),dtype=int)))
+    #返回所有数据及对应标签
+    return combined,y
+
+#获取停用词
+def loadStopWords():   
+    
+    stop = [line.strip()  for line in open('./stopWords.txt', 'r', encoding='utf-8').readlines() ]   
+    #print("type(loadStopWords_stop)",type(stop))
+    return stop  
+
+#对句子经行分词，并去掉换行符
+def cutWords(text):
+    
+    #分词
+    text = [jieba.lcut(document.replace('\n', '')) for document in text]
+    #读取分词后的数据，去除停用词后写入文本
+    f_o = open('./cutWords.txt', 'w', encoding='utf-8')
+    stopWords = loadStopWords()
+    for line in text: 
+        leftWords = []   
+        for i in line:  
+            if (i not in stopWords):  
+                leftWords.append(i)
+        text_str = ' '.join(leftWords)
+        f_o.write(text_str)
+        f_o.write('\n')
+    f_o.close()
+    #return text
+    
+    with open('./cutWords.txt','r',encoding='utf-8') as text_list:
+        return list(text_list)
+    
+#创建词语字典，并返回每个词语的索引，词向量，以及每个句子所对应的词语索引
+def create_dictionaries(model=None,
+                        combined=None):
+    ''' Function does are number of Jobs:
+        1-创建索引映射的单词
+        2-创建一个单词到矢量映射
+        3-转换训练和测试词典
+
+    '''
+    if (combined is not None) and (model is not None):
+        gensim_dict = Dictionary()
+        gensim_dict.doc2bow(model.wv.vocab.keys(),
+                            allow_update=True)
+        #  freqxiao10->0 所以k+1
+        w2indx = {v: k+1 for k, v in gensim_dict.items()}#所有频数超过10的词语的索引,(k->v)=>(v->k)
+        w2vec = {word: model[word] for word in w2indx.keys()}#所有频数超过10的词语的词向量, (word->model(word))
+
+        def parse_dataset(combined): # 闭包-->临时使用
+            ''' 单词变集合
+            '''
+            data=[]
+            for sentence in combined:
+                new_txt = []
+                for word in sentence:
+                    try:
+                        new_txt.append(w2indx[word])
+                    except:
+                        new_txt.append(0) # freqxiao10->0
+                data.append(new_txt)
+            return data # word=>index
+        combined=parse_dataset(combined)
+        combined= sequence.pad_sequences(combined, maxlen=maxlen)#每个句子所含词语对应的索引，所以句子中含有频数小于10的词语，索引为0
+        #返回每个词语的索引（从1开始编号）、词向量、每个句子所对应的词语索引
+        
+        return w2indx, w2vec,combined
+    else:
+        print ('没有提供数据...')
+
+
+#词向量训练
+def word2vec_train(combined):
+
+    model = Word2Vec(size=vocab_dim, #特征向量的维度，默认100，大的size需要更多数据
+                     min_count=n_exposures, #最小词频，小于该次数的单词被丢弃，默认5
+                     window=window_size, #当前词与预测词在一个句子中的最大距离
+                     workers=cpu_count, #参数训练的并行数
+                     iter=n_iterations) #迭代次数，默认5
+    #录入列表
+    model.build_vocab(combined) # input: list
+    #模型训练
+    model.train(combined,total_examples=model.corpus_count,epochs=model.iter)
+    #模型保存（语料的索引及词向量）
+    model.save('./Word2vec_model.pkl')
+    #每个词语的索引字典{单词：索引数字}##21088##、
+    #词向量字典{单词：词向量（100维长的数组）}##21088##、
+    #每个句子所对应的向量100维##总数据=21088*100##
+    index_dict, word_vectors,combined = create_dictionaries(model=model,combined=combined)
+
+    return   index_dict, word_vectors,combined
+
+
+def get_data(index_dict,word_vectors,combined,y):
+    #获取句子向量
+    n_symbols = len(index_dict) + 1  # 所有单词的索引数，频数小于10的词语索引为0，所以加1=8305
+    embedding_weights = np.zeros((n_symbols, vocab_dim)) # n_symbols*100的0矩阵
+    for word, index in index_dict.items(): # 从索引为1的词语开始，用词向量填充矩阵
+        embedding_weights[index, :] = word_vectors[word] #词向量矩阵，第一行是0向量
+    #随机抽取20%的测试集，x_test=20%，combined-样本特征集，y-样本标签，test_size-样本占比，都是列表
+    x_train, x_test, y_train, y_test = train_test_split(combined, y, test_size=0.2)
+    #多分类标签生成
+    y_train = keras.utils.to_categorical(y_train,num_classes=3) 
+    y_test = keras.utils.to_categorical(y_test,num_classes=3)
+
+    #字典长度##8305##、权重##字典中每个词1*100##、
+    #训练样本##16870*100##、训练样本标签##16870*3##、测试样本##4218*100##、测试样本标签##4218*3##
+    
+    return n_symbols,embedding_weights,x_train,y_train,x_test,y_test
+
+
+##定义网络结构,参数为get_data返回值
+def train_lstm(n_symbols,embedding_weights,x_train,y_train,x_test,y_test):
+    print ('定义一个简单的keras模型...')
+    #定义基本网络结构（输入维度100，输出维度3）
+    model = Sequential()  # or Graph or whatever
+#    #添加嵌入层，只能作为第一层，将输入转化为向量
+    model.add(Embedding(output_dim=vocab_dim, #大于0的整数，代表全连接嵌入的维度
+                        input_dim=n_symbols, #大或等于0的整数，字典长度，即输入数据最大下标+1 
+                        mask_zero=True, #布尔值，确定是否将输入中的‘0’看作是应该被忽略的‘填充’（padding）值，该参数在使用递归层处理变长输入时有用。
+                        weights=[embedding_weights], #初始化权值的numpy arrays组成的list
+                        input_length=input_length))  # 输入序列的长度
+    #添加LSTM层,内部投影的维数和最终输出的维数为50，激活函数tanh
+    model.add(LSTM(units=50, activation='tanh')) 
+    #添加GRU层
+    '''
+    model.add(GRU(units=50,
+                  activation='tanh',
+                  unroll=True,
+                  ))
+    '''
+    #添加RNN层
+#    model.add(SimpleRNN(units=50,
+#                  activation='tanh',
+#                  unroll=True,
+#                  ))
+
+    #采用50%的dropout,模型训练时随机让网络某些隐含层节点50%的权重不工作，避免过拟合（训练和测试数据的结果差别大）
+    model.add(Dropout(0.5))
+    #Dense=>全连接层,输出维度=3（三分类，输出维度为3，sigmoid）
+    model.add(Dense(3)) 
+    #最后一层用激活函数softmax
+    model.add(Activation('softmax'))
+    #二分类与多分类在前面的结构上都没有问题，就是需要改一下最后的全连接层，
+    #因为此时有3分类，所以需要Dense(3)，同时激活函数是softmax，如果是二分类就是dense(2)+sigmoid(激活函数)
+
+    print ('编译模型...')
+    #loss-目标函数（categorical_crossentropy-多分类，binary_crossentropy-二分类）
+    #optimizer-指定模型训练的优化器
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',metrics=['accuracy'])
+
+    print ("训练...") # batch_size=32
+    #用于训练一个固定迭代次数的模型
+    #x_train-训练数据，y_train-标签，batch_size-每次训练和梯度更新块的大小,epochs-迭代次数
+    #verbose-进度表示方式，0=不显示，1=显示进度条，2=只显示一个数据
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=n_epoch,verbose=1)
+
+    print ("评估...")
+    #展示模型在验证数据上的效果
+    #x_test-训练数据,y_test-标签,batch_size-更新块大小
+    score, acc = model.evaluate(x_test, y_test,
+                                batch_size=batch_size)
+    #以YAML字符串的形式返回模型，不包括权重，只包括结构
+    yaml_string = model.to_yaml()
+    with open('./lstm.yml', 'w') as outfile:
+        outfile.write( yaml.dump(yaml_string, default_flow_style=True) )
+    #将所有层的权值保存为HDF5文件
+    model.save_weights('./lstm.h5')
+    print ('Test score:', score)
+    print ('Test accuracy:', acc)
+
+
+#训练模型，并保存
+print ('加载数据...')
+combined,y=loadfile()
+
+print ("数据集：",len(combined),"，数据集标签：",len(y))
+
+print ('分词...')
+combined = cutWords(combined)
+f1 = open('./combined.txt','w',encoding='utf-8')
+for line in combined:
+    f1.write(str(line))
+f1.close()
+print ('训练Word2vec模型...')
+index_dict, word_vectors,combined=word2vec_train(combined)
+
+print ('获取网络参数...')
+n_symbols,embedding_weights,x_train,y_train,x_test,y_test=get_data(index_dict, word_vectors,combined,y)
+
+print ("训练集：",x_train.shape,"，训练集标签：",y_train.shape)
+print ("测试集：",x_test.shape,"，测试集标签：",y_test.shape)
+train_lstm(n_symbols,embedding_weights,x_train,y_train,x_test,y_test)
