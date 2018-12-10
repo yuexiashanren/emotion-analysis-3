0770357f989ca91d073aef0d9ff3bea8f8971eca
luanshiqiguo <1484836966@qq.com>
2018-12-10 18:59:06 +0800

    knowledge show

--- experiment/knowledge_content/show_content.py
+++ experiment/knowledge_content/show_content.py
@@ -1,7 +1,7 @@
 #! /bin/env python
 # -*- coding: utf-8 -*-
 """
-知识点内容情感分析
+符合每个章节知识点的情感分析
 """
 import jieba
 import numpy as np
@@ -14,7 +14,21 @@ from keras.models import model_from_yaml
 np.random.seed(1337)  # For Reproducibility
 import sys
 sys.setrecursionlimit(1000000)
+
 jieba.load_userdict('../../data/introductions.txt')
+jieba.load_userdict('../../data/emotions.txt')
+jieba.load_userdict('./units/unit1.txt')
+jieba.load_userdict('./units/unit2.txt')
+jieba.load_userdict('./units/unit3.txt')
+jieba.load_userdict('./units/unit4.txt')
+jieba.load_userdict('./units/unit5.txt')
+jieba.load_userdict('./units/unit6.txt')
+jieba.load_userdict('./units/unit7.txt')
+
+import numpy as np  
+import matplotlib.pyplot as plt  
+import matplotlib.gridspec as gridspec
+
 # 定义参数
 maxlen = 100
 
@@ -54,20 +68,48 @@ def create_dictionaries(model=None,
         print ('没有提供数据...')
 
 def loadStopWords():   
-    stop = [line.strip()  for line in open('../../data/stopWords.txt', 'r', encoding='utf-8').readlines() ]   
+    stop = [line.strip()  for line in open('../../lstm_test/stopWords.txt', 'r', encoding='utf-8').readlines() ]   
     #print("type(loadStopWords_stop)",type(stop))
     return stop
 
-def introductionWords():   
-    introduction = [line.strip()  for line in open('../../data/introductions.txt', 'r', encoding='utf-8').readlines() ]   
+def unit1Datas():   
+    unit_data = [line.strip()  for line in open('./units/unit1.txt', 'r', encoding='utf-8').readlines() ]   
     #print("type(loadStopWords_stop)",type(stop))
-    return introduction  
-f1 = open('./need_test.txt','w',encoding='utf-8')
-f2 = open('./text_str.txt','w',encoding='utf-8')
-f3 = open('./result_test.txt','w',encoding='utf-8')
-def input_transform(string):
+    return unit_data
+
+def unit2Datas():   
+    unit_data = [line.strip()  for line in open('./units/unit2.txt', 'r', encoding='utf-8').readlines() ]   
+    #print("type(loadStopWords_stop)",type(stop))
+    return unit_data
+
+def unit3Datas():   
+    unit_data = [line.strip()  for line in open('./units/unit3.txt', 'r', encoding='utf-8').readlines() ]   
+    #print("type(loadStopWords_stop)",type(stop))
+    return unit_data
+
+def unit4Datas():   
+    unit_data = [line.strip()  for line in open('./units/unit4.txt', 'r', encoding='utf-8').readlines() ]   
+    #print("type(loadStopWords_stop)",type(stop))
+    return unit_data
+
+def unit5Datas():   
+    unit_data = [line.strip()  for line in open('./units/unit5.txt', 'r', encoding='utf-8').readlines() ]   
+    #print("type(loadStopWords_stop)",type(stop))
+    return unit_data
+
+def unit6Datas():   
+    unit_data = [line.strip()  for line in open('./units/unit6.txt', 'r', encoding='utf-8').readlines() ]   
+    #print("type(loadStopWords_stop)",type(stop))
+    return unit_data
+
+def unit7Datas():   
+    unit_data = [line.strip()  for line in open('./units/unit7.txt', 'r', encoding='utf-8').readlines() ]   
+    #print("type(loadStopWords_stop)",type(stop))
+    return unit_data
+f3 = open('./need_test.txt','w',encoding='utf-8')
+#string表示初始的单个文本数据，unitDatas表示需要引入的第几单元的词典
+def input_transform(string,unitDatas):
     words=jieba.lcut(string)
-    #去除停用词
     stopWords = loadStopWords()
     leftWords = []
     for i in words:
@@ -76,30 +118,29 @@ def input_transform(string):
     text_str = leftWords
     #print("text_str",text_str)
     
-    f2.write(str(text_str))
-    f2.write('\n')
-
-    #筛选符合知识点的数据
+    #筛选符合章节信息的数据
     flag = "false"
-    introductions = introductionWords()
-    for i in text_str:
-        if i in introductions:
+    #引入单元词典，匹配符合该单元词典的数据
+    unit1s = unitDatas
+    for i in words:
+        if i in unit1s:
             flag = "true"
-            f3.write(str(text_str))
+            f3.write(str(words))
             f3.write('\n')
             break
+
     if flag == "true":
         words=np.array(text_str).reshape(1,-1)
-        f1.write(str(words))
-        f1.write('\n')
         #载入模型
         model=Word2Vec.load('../../lstm_test/model/Word2vec_model.pkl')
         _,_,combined=create_dictionaries(model,words)
         return combined
     else:
         return "none"
+f1 = open('./text_str.txt','w',encoding='utf-8')
+f2 = open('./result_test.txt','w',encoding='utf-8')
 
-def lstm_predict(string):
+def lstm_predict(string,unit,unitDatas):
     #print ('loading model...')
     with open('../../lstm_test/model/lstm.yml', 'r') as f:
         yaml_string = yaml.load(f)
@@ -116,31 +157,38 @@ def lstm_predict(string):
     sum = 0
     with open(string ,'r', encoding='utf-8') as ff:
         for line in ff.readlines():
-            data=input_transform(line)
+            #line表示输入的单个文本数据，unitDatas表示需要引入的单元词典
+            data = input_transform(line,unitDatas)
             if data != "none":
+                f1.write(str(line))
+                f2.write(str(data))
+                f2.write('\n')
                 data.reshape(1,-1)
                 #print data
-                result=model.predict_classes(data)
+                result = model.predict_classes(data)
                 choose = result[0]
-                print(sum+1,choose)
-                if choose==1:
+                #print(sum+1,choose)
+                if choose == 1:
                     pos += 1
                     sum += 1
-                elif choose==0:
+                elif choose == 0:
                     neu += 1
                     sum += 1
                 else:
                     neg += 1
                     sum += 1
+
+    print("单元",unit)
     print("sum",sum)
     print("negative:",neg)
     print("neural",neu)
     print("positive",pos)
-    print('negative/sum: {:.2%}'.format(neg/sum))
-    print('neural/sum: {:.2%}'.format(neu/sum))
-    print('positive/sum: {:.2%}'.format(pos/sum))
-
-if __name__=='__main__':
     
-    string = './input_test.txt'
-    lstm_predict(string)       
+if __name__=='__main__':
+     
+    route = './input_test.txt'
+    for i in range(1,8):
+        unit = [unit1Datas(), unit2Datas(), unit3Datas(), unit4Datas(), unit5Datas(), unit6Datas(), unit7Datas()]
+        #route表示输入文本路径，i表示第几章节，unit[]表示需要的第几章节的词典
+        result = lstm_predict(route,i,unit[i-1])
+    
