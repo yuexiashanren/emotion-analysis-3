b72aab9b8841a86df4ff670c66088925704f57d7
luanshiqiguo <1484836966@qq.com>
2019-07-02 16:40:12 +0800

    0702

--- lstm_predict/new_test/cluster.py
+++ lstm_predict/new_test/cluster.py
@@ -14,17 +14,27 @@ plt.plot(dataset)
 plt.show()
 '''
 # load the dataset
-#原有两列，一列是时间，一列是乘客数量，这里利用usecols=[1],只取了乘客数量一列
-dataframe = pandas.read_csv('international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
+#取第一列
+dataframe = pandas.read_csv('periodData.csv', usecols=[0], engine='python', skipfooter=1)
 #通过.values得到dataframe的值，返回shape是(144, 1)的数组形式
 dataset = dataframe.values 
 #把dataset里的所有数据从整型变为浮点型
 dataset = dataset.astype('float32') 
 
 print("dataframe:", dataframe)
-print("dataset:", dataset)
-
+#print("dataset:", dataset)
+setValue = dataset
 #dataset数据归一化，[0, 1]
 scaler = MinMaxScaler(feature_range=(-1, 1))
 dataset = scaler.fit_transform(dataset)
-print("new dataset:", dataset)
+print("new dataset:")
+classes = ""
+for i in range(len(dataset)):
+	if dataset[i] <= -0.5:
+		classes = "负面"
+	elif dataset[i] < 0.5:
+		classes = "一般"
+	else:
+		classes = "正面"
+	#print("dataset["+str(i)+"]=", dataset[i], classes)
+	print(setValue[i], dataset[i], classes)
